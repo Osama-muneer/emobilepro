a6ee55e48d6c506d8dbe2c5a15bbb5870744819e
AMJADAHMED2024 <amjad.ahmed@sys-es.com>
2025-06-03 17:42:01 +0300

    MODFIY ON DATE 2025/06/03    BY ENG.AMJAD

--- lib/Services/EasyLoadingService.dart
+++ lib/Services/EasyLoadingService.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'LogMobService.dart';  // استيراد خدمة حفظ السجلات
 
 class EasyLoadingService {
+  EasyLoadingService._(); // Private constructor
+
   static void init() {
     EasyLoading.instance
       ..displayDuration = const Duration(milliseconds: 2000)
@@ -17,19 +20,40 @@ class EasyLoadingService {
       ..dismissOnTap = false;
   }
 
-  static void showError(String message) {
-    EasyLoading.showError(message);
-  }
-
-  static void showSuccess(String message) {
-    EasyLoading.showSuccess(message);
-  }
-
+  /// Show a loading indicator and log to database
   static void showLoading([String? message]) {
     EasyLoading.show(status: message ?? 'جاري التحميل...');
+    _logToDatabase(message ?? 'جاري التحميل...',LogType.INF);
   }
 
+  /// Show a success message and log to database
+  static void showSuccess(
+      String message, {
+        Duration duration = const Duration(milliseconds: 2000),
+      }) {
+    EasyLoading.showSuccess(message, duration: duration);
+    _logToDatabase(message,LogType.SUC);
+  }
+
+  /// Show an error message and log to database
+  static void showError(
+      String message, {
+        Duration duration = const Duration(milliseconds: 2000),
+      }) {
+    EasyLoading.showError(message, duration: duration);
+    _logToDatabase(message,LogType.ERR);
+  }
+
+  /// Dismiss the loading or message
   static void dismiss() {
     EasyLoading.dismiss();
+  }
+
+  /// Private method to log messages to database
+  static Future<void> _logToDatabase(String message,LogType type) async {
+    await MobLogDatabase().log(
+      type: type, // You can adjust the LogType here (e.g., LogType.ERR or LogType.SUC)
+      detail: message,
+    );
   }
 }
